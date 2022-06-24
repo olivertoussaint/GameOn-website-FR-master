e375a450e50dae199955a8540083af7bf1ddbc03
olivertoussaint <toussaint.olivier66@gmail.com>
2022-06-24 16:55:30 +0200

    add a function to close the modal

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+modalBtnClose.forEach((btnClose) => btnClose.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
